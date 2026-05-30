d25d2ac8fa66ef8c172fc3f69b360f472fcf387a
Palash rai <palashr2612@gmail.com>
2026-05-30 14:22:16 +0530

    feat: implement side panel toggle and data synchronization in background script

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,15 @@
 // background.js
 
+chrome.action.onClicked.addListener((tab) => {
+  try {
+    if (chrome.sidePanel && chrome.sidePanel.open) {
+      chrome.sidePanel.open({ windowId: tab.windowId });
+    }
+  } catch (e) {
+    console.warn("Manual side panel open not supported:", e);
+  }
+});
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'SYNC_APPLYBUDDY_DATA') {
     chrome.storage.local.set({ applybuddy_data: request.data }, () => {
